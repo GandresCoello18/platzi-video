2d70800ace2217a184f99aee33559b5a3817370d
GandresCoello18 <goyeselcoca@gmail.com>
2019-10-24 23:35:17 -0500

    agregando nuevos cambios

---
+++ src/component/holaMundo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/app.scss";
+
+const Hola = props => {
+    return(
+        <>
+            <h1>hola mundo</h1>
+        </>
+    );
+}
+
+export default Hola;
